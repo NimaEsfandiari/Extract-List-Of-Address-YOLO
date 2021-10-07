d083267681d54a92b121b6e0d7c5a26d771b7266
NimaEsfandiari <77873900+NimaEsfandiari@users.noreply.github.com>
2021-10-07 17:14:47 +0330

    Create listOfAddress.py

---
+++ listOfAddress.py
@@ -0,0 +1,16 @@
+from os import listdir
+
+
+base_url='/data/images'
+
+def test():
+    listOfData = listdir('./images')   
+    for data in listOfData:
+        formatFile = data.split('.')[-1]
+        if formatFile == 'jpg':
+            with open('trian.txt', 'a') as f:
+                f.write(base_url + data + '\n')
+                
+
+if __name__ == '__main__':
+    test()
